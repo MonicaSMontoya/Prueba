e2869d383683b8c0df5581096091b60d5b850331
MonicaSanchez <monicalmontoya@gmail.com>
2016-08-02 21:51:37 +0200

    mi primer commit

---
+++ sw-precache-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/images/*',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+  ],
+  navigateFallback: '/index.html'
+};
